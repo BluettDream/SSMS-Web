cd8792a04c2cd8477c5fca36da154afbeb5eb300
MengHao Tian <bluetian1st@gmail.com>
2023-05-07 17:41:46 +0800

    Update 更改网页ruoyi大标题

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="register">
     <el-form ref="registerRef" :model="registerForm" :rules="registerRules" class="register-form">
-      <h3 class="title">RuoYi-Vue-Plus后台管理系统</h3>
+      <h3 class="title">SSMS学生成绩管理系统</h3>
       <el-form-item prop="username">
         <el-input 
           v-model="registerForm.username" 
