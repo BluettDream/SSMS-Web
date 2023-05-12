0633676a0ac8aa382749c3ba5787f56c37166dc6
MengHao Tian <bluetian1st@gmail.com>
2023-05-12 09:20:45 +0800

    update 格式化文件

--- src/views/ssms/course/index.vue
+++ src/views/ssms/course/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryRef"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="教师编号" prop="userName" v-if="isAdmin">
         <el-input
           v-model="queryParams.userName"
@@ -33,19 +39,24 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="课程时间" style="width: 308px;">
+      <el-form-item label="课程时间" style="width: 308px">
         <el-date-picker
-            v-model="dateRange"
-            value-format="YYYY-MM-DD HH:mm:ss"
-            type="daterange"
-            range-separator="-"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-            :default-time="[new Date(2000, 1, 1, 0, 0, 0), new Date(2000, 1, 1, 23, 59, 59)]"
+          v-model="dateRange"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          :default-time="[
+            new Date(2000, 1, 1, 0, 0, 0),
+            new Date(2000, 1, 1, 23, 59, 59),
+          ]"
         ></el-date-picker>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
+        <el-button type="primary" icon="Search" @click="handleQuery"
+          >搜索</el-button
+        >
         <el-button icon="Refresh" @click="resetQuery">重置</el-button>
       </el-form-item>
     </el-form>
@@ -58,7 +69,8 @@
           icon="Plus"
           @click="handleAdd"
           v-hasPermi="['ssms:course:add']"
-        >新增</el-button>
+          >新增</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -68,7 +80,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['ssms:course:edit']"
-        >修改</el-button>
+          >修改</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -78,16 +91,18 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['ssms:course:remove']"
-        >删除</el-button>
+          >删除</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
-            type="info"
-            plain
-            icon="Upload"
-            @click="handleImport"
-            v-hasPermi="['ssms:course:import']"
-        >导入</el-button>
+          type="info"
+          plain
+          icon="Upload"
+          @click="handleImport"
+          v-hasPermi="['ssms:course:import']"
+          >导入</el-button
+        >
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -96,37 +111,78 @@
           icon="Download"
           @click="handleExport"
           v-hasPermi="['ssms:course:export']"
-        >导出</el-button>
+          >导出</el-button
+        >
       </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        v-model:showSearch="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="courseList" @selection-change="handleSelectionChange">
+    <el-table
+      v-loading="loading"
+      :data="courseList"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="课程编号" align="center" prop="courseId" v-if="true"/>
+      <el-table-column
+        label="课程编号"
+        align="center"
+        prop="courseId"
+        v-if="true"
+      />
       <el-table-column label="教师姓名" align="center" prop="nickName" />
       <el-table-column label="课程名称" align="center" prop="courseName" />
       <el-table-column label="学分" align="center" prop="credit" />
-      <el-table-column label="开始日期" align="center" prop="startTime" width="180">
+      <el-table-column
+        label="开始日期"
+        align="center"
+        prop="startTime"
+        width="180"
+      >
         <template #default="scope">
-          <span>{{ parseTime(scope.row.startTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.startTime, "{y}-{m}-{d}") }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="结束日期" align="center" prop="finishTime" width="180">
+      <el-table-column
+        label="结束日期"
+        align="center"
+        prop="finishTime"
+        width="180"
+      >
         <template #default="scope">
-          <span>{{ parseTime(scope.row.finishTime, '{y}-{m}-{d}') }}</span>
+          <span>{{ parseTime(scope.row.finishTime, "{y}-{m}-{d}") }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-          <template #default="scope">
-              <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['ssms:course:edit']">修改</el-button>
-              <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['ssms:course:remove']">删除</el-button>
-          </template>
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
+        <template #default="scope">
+          <el-button
+            link
+            type="primary"
+            icon="Edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['ssms:course:edit']"
+            >修改</el-button
+          >
+          <el-button
+            link
+            type="primary"
+            icon="Delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['ssms:course:remove']"
+            >删除</el-button
+          >
+        </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       v-model:page="queryParams.pageNum"
       v-model:limit="queryParams.pageSize"
@@ -134,7 +190,13 @@
     />
 
     <!-- 添加或修改课程信息对话框 -->
-    <el-dialog draggable :title="title" v-model="open" width="500px" append-to-body>
+    <el-dialog
+      draggable
+      :title="title"
+      v-model="open"
+      width="500px"
+      append-to-body
+    >
       <el-form ref="courseRef" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="教师编号" prop="userName">
           <el-input v-model="form.userName" placeholder="请输入教师编号" />
@@ -146,68 +208,94 @@
           <el-input v-model="form.credit" placeholder="请输入学分" />
         </el-form-item>
         <el-form-item label="开始日期" prop="startTime">
-          <el-date-picker clearable
+          <el-date-picker
+            clearable
             v-model="form.startTime"
             type="datetime"
             value-format="YYYY-MM-DD HH:mm:ss"
-            placeholder="请选择开始日期">
+            placeholder="请选择开始日期"
+          >
           </el-date-picker>
         </el-form-item>
         <el-form-item label="结束日期" prop="finishTime">
-          <el-date-picker clearable
+          <el-date-picker
+            clearable
             v-model="form.finishTime"
             type="datetime"
             value-format="YYYY-MM-DD HH:mm:ss"
-            placeholder="请选择结束日期">
+            placeholder="请选择结束日期"
+          >
           </el-date-picker>
         </el-form-item>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
-          <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+          <el-button :loading="buttonLoading" type="primary" @click="submitForm"
+            >确 定</el-button
+          >
           <el-button @click="cancel">取 消</el-button>
         </div>
       </template>
     </el-dialog>
 
     <!-- 课程导入对话框 -->
-    <el-dialog draggable :title="upload.title" v-model="upload.open" width="400px" append-to-body>
-         <el-upload
-            ref="uploadRef"
-            :limit="1"
-            accept=".xlsx, .xls"
-            :headers="upload.headers"
-            :action="upload.url + '?updateSupport=' + upload.updateSupport"
-            :disabled="upload.isUploading"
-            :on-progress="handleFileUploadProgress"
-            :on-success="handleFileSuccess"
-            :auto-upload="false"
-            drag
-         >
-            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-            <template #tip>
-               <div class="el-upload__tip text-center">
-                  <div class="el-upload__tip">
-                     <el-checkbox v-model="upload.updateSupport" />是否更新已经存在的课程数据
-                  </div>
-                  <span>仅允许导入xls、xlsx格式文件。</span>
-                  <el-link type="primary" :underline="false" style="font-size:12px;vertical-align: baseline;" @click="importTemplate">下载模板</el-link>
-               </div>
-            </template>
-         </el-upload>
-         <template #footer>
-            <div class="dialog-footer">
-               <el-button type="primary" @click="submitFileForm">确 定</el-button>
-               <el-button @click="upload.open = false">取 消</el-button>
+    <el-dialog
+      draggable
+      :title="upload.title"
+      v-model="upload.open"
+      width="400px"
+      append-to-body
+    >
+      <el-upload
+        ref="uploadRef"
+        :limit="1"
+        accept=".xlsx, .xls"
+        :headers="upload.headers"
+        :action="upload.url + '?updateSupport=' + upload.updateSupport"
+        :disabled="upload.isUploading"
+        :on-progress="handleFileUploadProgress"
+        :on-success="handleFileSuccess"
+        :auto-upload="false"
+        drag
+      >
+        <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <template #tip>
+          <div class="el-upload__tip text-center">
+            <div class="el-upload__tip">
+              <el-checkbox
+                v-model="upload.updateSupport"
+              />是否更新已经存在的课程数据
             </div>
-         </template>
-      </el-dialog>
+            <span>仅允许导入xls、xlsx格式文件。</span>
+            <el-link
+              type="primary"
+              :underline="false"
+              style="font-size: 12px; vertical-align: baseline"
+              @click="importTemplate"
+              >下载模板</el-link
+            >
+          </div>
+        </template>
+      </el-upload>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitFileForm">确 定</el-button>
+          <el-button @click="upload.open = false">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup name="Course">
-import { listCourse, getCourse, delCourse, addCourse, updateCourse } from "@/api/ssms/course";
+import {
+  listCourse,
+  getCourse,
+  delCourse,
+  addCourse,
+  updateCourse,
+} from "@/api/ssms/course";
 import { checkRole } from "@/utils/permission";
 import { getToken } from "@/utils/auth";
 
@@ -223,7 +311,7 @@ const single = ref(true);
 const multiple = ref(true);
 const total = ref(0);
 const title = ref("");
-const isAdmin = ref(checkRole(['admin']))
+const isAdmin = ref(checkRole(["admin"]));
 const dateRange = ref([]);
 /*** 课程导入参数 */
 const upload = reactive({
@@ -238,7 +326,7 @@ const upload = reactive({
   // 设置上传的请求头部
   headers: { Authorization: "Bearer " + getToken() },
   // 上传的地址
-  url: import.meta.env.VITE_APP_BASE_API + "/ssms/course/importData"
+  url: import.meta.env.VITE_APP_BASE_API + "/ssms/course/importData",
 });
 const data = reactive({
   form: {},
@@ -254,24 +342,22 @@ const data = reactive({
   },
   rules: {
     courseId: [
-      { required: true, message: "课程编号不能为空", trigger: "blur" }
+      { required: true, message: "课程编号不能为空", trigger: "blur" },
     ],
     userName: [
-      { required: true, message: "教师编号不能为空", trigger: "blur" }
+      { required: true, message: "教师编号不能为空", trigger: "blur" },
     ],
     courseName: [
-      { required: true, message: "课程名称不能为空", trigger: "blur" }
+      { required: true, message: "课程名称不能为空", trigger: "blur" },
     ],
-    credit: [
-      { required: true, message: "学分不能为空", trigger: "blur" }
-    ],
+    credit: [{ required: true, message: "学分不能为空", trigger: "blur" }],
     startTime: [
-      { required: true, message: "开始日期不能为空", trigger: "blur" }
+      { required: true, message: "开始日期不能为空", trigger: "blur" },
     ],
     finishTime: [
-      { required: true, message: "结束日期不能为空", trigger: "blur" }
+      { required: true, message: "结束日期不能为空", trigger: "blur" },
     ],
-  }
+  },
 });
 
 const { queryParams, form, rules } = toRefs(data);
@@ -279,7 +365,7 @@ const { queryParams, form, rules } = toRefs(data);
 /** 查询课程信息列表 */
 function getList() {
   loading.value = true;
-  listCourse(queryParams.value).then(response => {
+  listCourse(queryParams.value).then((response) => {
     courseList.value = response.rows;
     total.value = response.total;
     loading.value = false;
@@ -306,7 +392,7 @@ function reset() {
     createBy: null,
     createTime: null,
     updateBy: null,
-    updateTime: null
+    updateTime: null,
   };
   proxy.resetForm("courseRef");
 }
@@ -314,10 +400,10 @@ function reset() {
 /** 搜索按钮操作 */
 function handleQuery() {
   queryParams.value.pageNum = 1;
-  if(dateRange && dateRange.value !== null && dateRange.value.length > 0){
+  if (dateRange && dateRange.value !== null && dateRange.value.length > 0) {
     queryParams.value.startTime = dateRange.value[0];
     queryParams.value.finishTime = dateRange.value[1];
-  }else{
+  } else {
     queryParams.value.startTime = undefined;
     queryParams.value.finishTime = undefined;
   }
@@ -333,7 +419,7 @@ function resetQuery() {
 
 // 多选框选中数据
 function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.courseId);
+  ids.value = selection.map((item) => item.courseId);
   single.value = selection.length != 1;
   multiple.value = !selection.length;
 }
@@ -347,10 +433,10 @@ function handleAdd() {
 
 /** 修改按钮操作 */
 function handleUpdate(row) {
-  loading.value = true
+  loading.value = true;
   reset();
-  const _courseId = row.courseId || ids.value
-  getCourse(_courseId).then(response => {
+  const _courseId = row.courseId || ids.value;
+  getCourse(_courseId).then((response) => {
     loading.value = false;
     form.value = response.data;
     open.value = true;
@@ -360,25 +446,29 @@ function handleUpdate(row) {
 
 /** 提交按钮 */
 function submitForm() {
-  proxy.$refs["courseRef"].validate(valid => {
+  proxy.$refs["courseRef"].validate((valid) => {
     if (valid) {
       buttonLoading.value = true;
       if (form.value.courseId != null) {
-        updateCourse(form.value).then(response => {
-          proxy.$modal.msgSuccess("修改成功");
-          open.value = false;
-          getList();
-        }).finally(() => {
-          buttonLoading.value = false;
-        });
+        updateCourse(form.value)
+          .then((response) => {
+            proxy.$modal.msgSuccess("修改成功");
+            open.value = false;
+            getList();
+          })
+          .finally(() => {
+            buttonLoading.value = false;
+          });
       } else {
-        addCourse(form.value).then(response => {
-          proxy.$modal.msgSuccess("新增成功");
-          open.value = false;
-          getList();
-        }).finally(() => {
-          buttonLoading.value = false;
-        });
+        addCourse(form.value)
+          .then((response) => {
+            proxy.$modal.msgSuccess("新增成功");
+            open.value = false;
+            getList();
+          })
+          .finally(() => {
+            buttonLoading.value = false;
+          });
       }
     }
   });
@@ -387,36 +477,47 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _courseIds = row.courseId || ids.value;
-  proxy.$modal.confirm('是否确认删除课程信息编号为"' + _courseIds + '"的数据项？').then(function() {
-    loading.value = true;
-    return delCourse(_courseIds);
-  }).then(() => {
-    loading.value = true;
-    getList();
-    proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {
-  }).finally(() => {
-    loading.value = false;
-  });
+  proxy.$modal
+    .confirm('是否确认删除课程信息编号为"' + _courseIds + '"的数据项？')
+    .then(function () {
+      loading.value = true;
+      return delCourse(_courseIds);
+    })
+    .then(() => {
+      loading.value = true;
+      getList();
+      proxy.$modal.msgSuccess("删除成功");
+    })
+    .catch(() => {})
+    .finally(() => {
+      loading.value = false;
+    });
 }
 
 /** 导出按钮操作 */
 function handleExport() {
-  proxy.download('ssms/course/export', {
-    ...queryParams.value
-  }, `course_${new Date().getTime()}.xlsx`)
+  proxy.download(
+    "ssms/course/export",
+    {
+      ...queryParams.value,
+    },
+    `course_${new Date().getTime()}.xlsx`
+  );
 }
 
 /** 导入按钮操作 */
 function handleImport() {
   upload.title = "用户导入";
   upload.open = true;
-};
+}
 /** 下载模板操作 */
 function importTemplate() {
-  proxy.download("ssms/course/importTemplate", {
-  }, `course_template_${new Date().getTime()}.xlsx`);
-};
+  proxy.download(
+    "ssms/course/importTemplate",
+    {},
+    `course_template_${new Date().getTime()}.xlsx`
+  );
+}
 /**文件上传中处理 */
 const handleFileUploadProgress = (event, file, fileList) => {
   upload.isUploading = true;
@@ -426,13 +527,19 @@ const handleFileSuccess = (response, file, fileList) => {
   upload.open = false;
   upload.isUploading = false;
   proxy.$refs["uploadRef"].handleRemove(file);
-  proxy.$alert("<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" + response.msg + "</div>", "导入结果", { dangerouslyUseHTMLString: true });
+  proxy.$alert(
+    "<div style='overflow: auto;overflow-x: hidden;max-height: 70vh;padding: 10px 20px 0;'>" +
+      response.msg +
+      "</div>",
+    "导入结果",
+    { dangerouslyUseHTMLString: true }
+  );
   getList();
 };
 /** 提交上传文件 */
 function submitFileForm() {
   proxy.$refs["uploadRef"].submit();
-};
+}
 
 getList();
 </script>
