235b1360b7182eb3bf63d56c4cc2d5a58b4b5f4a
MengHao Tian <bluetian1st@gmail.com>
2023-05-30 11:34:24 +0800

    update 删除自动填充管理员账号和密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -80,8 +80,8 @@ const router = useRouter();
 const { proxy } = getCurrentInstance();
 
 const loginForm = ref({
-  username: "admin",
-  password: "admin123",
+  username: "",
+  password: "",
   rememberMe: false,
   code: "",
   uuid: ""
